bd98e4350f523d7fc8d23993d93b8765b1def463
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-15 18:33:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -173,6 +173,6 @@ def play(board)
       winner(board) == "X" || winner(board) == "O"
       puts "Congratulations #{winner(board)}!"
    elsif draw?(board)
-      puts "Cats Game!"
+      puts "Cat's Game!"
    end
 end
