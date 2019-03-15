733763b157e966a577ef39a42ccdc92e7cdcdd12
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-15 18:34:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,41 +130,6 @@ def endgame(board)
   end
 end
 
-#Define your play method below
-# def play(board)
-#   until over?(board)
-#     turn(board)
-#     if winner(board) == "X"
-#       puts "Congratulations X!"
-#     elsif winner(board) == "O"
-#       puts "Congratulations O!"
-#     elsif draw?(board)
-#       puts "Cat's Game!"
-#     end
-#   end
-# end
-
-# def play(board)
-#   if winner(board) == "X"
-#     puts "Congratulations X!"
-#   elsif winner(board) == "O"
-#     puts "Congratulations O!"
-#   elsif draw?(board)
-#     puts "Cat's Game!"
-#   else
-#     until over?(board)
-#       turn(board)
-#       if winner(board) == "X"
-#         puts "Congratulations X!"
-#       elsif winner(board) == "O"
-#         puts "Congratulations O!"
-#       elsif draw?(board)
-#         puts "Cat's Game!"
-#       end
-#     end
-#   end
-# end
-
 def play(board)
    until over?(board)
       turn(board)
